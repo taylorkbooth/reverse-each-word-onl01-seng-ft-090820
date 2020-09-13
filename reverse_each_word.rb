b736af1a949bc011d185d70de1ed1e80d95a5624
Taylor Booth <taylorkbooth@gmail.com>
2020-09-13 04:24:16 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+# def reverse_each_word str
+#   str.split.reverse.join(' ')
+# end
+#   reverse_each_word("Hello there, and how are you?")
+  
+  
+  
+# ["Hello ", "there, ", "and ", "how ", "are ", "you?"]
+
+# def reverse_each_word str
+#   newarray = []
+#   str = "Hello there, and how are you?"
+#   print str.split(",")
+# end
+
+
+def reverse_each_word(string="Hello there, and how are you?")
